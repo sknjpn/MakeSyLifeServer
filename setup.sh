bdf4c7622ccb9155526ffe876324f98d7a8cfd25
sknjpn <sknjpn@gmail.com>
2022-10-11 19:08:56 +0900

    fix mirror server

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 cd
 
+sudo perl -p -i.bak -e 's%(deb(?:-src|)\s+)https?://(?!archive\.canonical\.com|security\.ubuntu\.com)[^\s]+%$1http://ftp.jaist.ac.jp/pub/Linux/ubuntu/%' /etc/apt/sources.list
+
 # Update Packages
 sudo apt update && sudo apt upgrade -y
 
